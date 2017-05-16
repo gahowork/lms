6447295d28a7175b0525cfe79e81f3564e51151d
Gerhard Ganneshofer <spamme@gaho.at>
2017-05-16 21:33:40 +0200

    adding new controller for the config menu

---
+++ gui/php/cakephp/src/Controller/ConfigsController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Configs Controller
+ *
+ */
+class ConfigsController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function index()
+    {
+    }
+
+}
