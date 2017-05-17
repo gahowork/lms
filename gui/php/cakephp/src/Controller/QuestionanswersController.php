c0337e70b9f381e0760e8a0d6ed98bb2aadbd2e3
Gerhard Ganneshofer <spamme@gaho.at>
2017-05-17 23:49:02 +0200

    new methods to modify questionanswers by a specific question

--- gui/php/cakephp/src/Controller/QuestionanswersController.php
+++ gui/php/cakephp/src/Controller/QuestionanswersController.php
@@ -44,6 +44,23 @@ class QuestionanswersController extends AppController
         $this->set('_serialize', ['questionanswer']);
     }
 
+	/**
+	 * Viewbyquestion method
+	 *
+	 * @param string|null $id Questionanswer id.
+	 * @return \Cake\Network\Response|null
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function viewbyquestion($id = null)
+	{
+		$questionanswer = $this->Questionanswers->get($id, [
+			'contain' => ['Questions', 'Answers']
+		]);
+
+		$this->set('questionanswer', $questionanswer);
+		$this->set('_serialize', ['questionanswer']);
+	}
+
     /**
      * Add method
      *
@@ -58,6 +75,30 @@ class QuestionanswersController extends AppController
                 $this->Flash->success(__('The questionanswer has been saved.'));
 
                 return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The questionanswer could not be saved. Please, try again.'));
+        }
+        $questions = $this->Questionanswers->Questions->find('list', ['limit' => 200]);
+        $answers = $this->Questionanswers->Answers->find('list', ['limit' => 200]);
+        $this->set(compact('questionanswer', 'questions', 'answers'));
+        $this->set('_serialize', ['questionanswer']);
+    }
+
+	/**
+     * Addbyquestion method
+     *
+     * @return \Cake\Network\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function addbyquestion($id = null)
+    {
+        $questionanswer = $this->Questionanswers->newEntity();
+		$questionanswer->question_id = $id;
+        if ($this->request->is('post')) {
+            $questionanswer = $this->Questionanswers->patchEntity($questionanswer, $this->request->data);
+            if ($this->Questionanswers->save($questionanswer)) {
+                $this->Flash->success(__('The questionanswer has been saved.'));
+
+                return $this->redirect(['controller'=>'questions', 'action' => 'view', $questionanswer->question_id]);
             }
             $this->Flash->error(__('The questionanswer could not be saved. Please, try again.'));
         }
@@ -94,6 +135,37 @@ class QuestionanswersController extends AppController
         $this->set('_serialize', ['questionanswer']);
     }
 
+	/**
+     * Editbyquestion method
+     *
+     * @param string|null $id Questionanswer id.
+     * @return \Cake\Network\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function editbyquestion($id = null)
+    {
+        $questionanswer = $this->Questionanswers->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+			debug($questionanswer);
+			debug($this->request->data);
+            $questionanswer = $this->Questionanswers->patchEntity($questionanswer, $this->request->data);
+			debug($questionanswer);
+
+            if ($this->Questionanswers->save($questionanswer)) {
+                $this->Flash->success(__('The questionanswer has been saved.'));
+
+                return $this->redirect(['controller'=>'questionanswers', 'action' => 'viewbyquestion', $questionanswer->id]);
+            }
+            $this->Flash->error(__('The questionanswer could not be saved. Please, try again.'));
+        }
+        $questions = $this->Questionanswers->Questions->find('list', ['limit' => 200]);
+        $answers = $this->Questionanswers->Answers->find('list', ['limit' => 200]);
+        $this->set(compact('questionanswer', 'questions', 'answers'));
+        $this->set('_serialize', ['questionanswer']);
+    }
+
     /**
      * Delete method
      *
@@ -112,5 +184,25 @@ class QuestionanswersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+	/**
+     * Deletebyquestion method
+     *
+     * @param string|null $id Questionanswer id.
+     * @return \Cake\Network\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function deletebyquestion($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $questionanswer = $this->Questionanswers->get($id);
+        if ($this->Questionanswers->delete($questionanswer)) {
+            $this->Flash->success(__('The questionanswer has been deleted.'));
+        } else {
+            $this->Flash->error(__('The questionanswer could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['controller'=>'questions', 'action' => 'view', $questionanswer->question_id]);
     }
 }
